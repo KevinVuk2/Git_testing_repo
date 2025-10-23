499d01f2a2e95dfe1f6d4b36a6ca8dd5e2e06999
Kevin Vu <datvuk2@gmail.com>
2025-10-23 15:07:14 +0200

    This is my fourth commit

--- First_file.py
+++ First_file.py
@@ -3,3 +3,6 @@ print("Hello, World!")
 
 #This  is the second line of code
 print("This is a test for git commit functionality.")
+
+# This is a change to our branch
+print("This line was added in a new branch.")
